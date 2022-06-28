387975ff43043c0d6e5745a4e1b6f3e00a852e0b
andrewkosyak <testing.qa.30.group@gmail.com>
2022-06-28 23:51:19 +0300

    add hw1(advanced)

---
+++ HW_1(advanced).js
@@ -0,0 +1,121 @@
+// 1*:
+// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+// Пример: const checkAge = function(age) {
+// Ваши преобразования
+// }
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61
+
+
+age_2 = 18; 
+age_3 = 60;
+
+const checkAge = function(age){
+    if(age < age_2) {
+        console.log("You don’t have access cause your age is " + age + " It's less then " + age_2)
+    } else if (age >=  age_2, age <  age_3) {
+        console.log('Welcome  !')
+    } else if (age  > age_3) {
+        console.log("Keep calm and look Culture channel")
+    } else {
+        console.log("Technical work")
+    }
+}
+
+checkAge(17)
+checkAge(18)
+checkAge(60)
+checkAge(61)
+
+
+// 2*:
+// Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных.
+//  И если он не Number - кидалась ошибка.
+
+
+age_2 = 18; 
+age_3 = 60;
+
+const checkAge = function(age){
+    age = Number(age);
+    if (age){
+        if (age < age_2) {
+        console.log("You don’t have access cause your age is " + age + " It's less then " + age_2)
+    } else if (age >=  age_2, age <  age_3) {
+        console.log('Welcome  !')
+    } else if (age  > age_3) {
+        console.log("Keep calm and look Culture channel")
+    } else {
+        console.log("Technical work") 
+    }
+ } else {
+        console.log('not a number')
+    }
+}
+
+checkAge(17)
+checkAge(18)
+checkAge('dsadas')
+checkAge(60)
+checkAge(61)
+checkAge('0')
+
+// // 3**:
+// // Преобразовать 2* таким образом, чтобы значение '2' 
+// // (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+
+age_2 = 18; 
+age_3 = 60;
+
+const checkAge = function(age){
+        age = Number(age);
+    if (!isNaN(age)){
+        if (age < age_2) {
+        console.log("You don’t have access cause your age is " + age + " It's less then " + age_2)
+    } else if (age >=  age_2, age <  age_3) {
+        console.log('Welcome  !')
+    } else if (age  > age_3) {
+        console.log("Keep calm and look Culture channel")
+    } else {
+        console.log("Technical work") 
+    }
+ } else {
+        console.log('not a number')
+    }
+}
+
+checkAge(17)
+checkAge(18)
+checkAge('dsadas')
+checkAge(60)
+checkAge(61)
+checkAge('0')
+checkAge('2')
+
+
+
+// 4***:
+// Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
+
+age_2 = 18; 
+age_3 = 60;
+
+const checkAge = function(age){
+        age = Number(age);
+    if (!isNaN(age)){
+        if (age < age_2) {
+        alert("You don’t have access cause your age is " + age + " It's less then " + age_2)
+    } else if (age >=  age_2, age <  age_3) {
+        alert('Welcome  !')
+    } else if (age  > age_3) {
+        alert("Keep calm and look Culture channel")
+    } else {
+        alert("Technical work") 
+    }
+ } else {
+    alert('not a number')
+    }
+}
+
+let q = prompt('Enter your age')
+checkAge(q)
